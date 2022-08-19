d3de8089900c80c92f726f15ea82add6ea54be3d
duilec <elelsyqrlc42@163.com>
2022-08-19 11:52:16 +0800

    done with GuitarHeroLite and GuitarString in proj1

--- proj1/gh2/GuitarHeroLite.java
+++ proj1/gh2/GuitarHeroLite.java
@@ -6,35 +6,46 @@ import edu.princeton.cs.algs4.StdDraw;
  * A client that uses the synthesizer package to replicate a plucked guitar string sound
  */
 public class GuitarHeroLite {
-    public static final double CONCERT_A = 440.0;
-    public static final double CONCERT_C = CONCERT_A * Math.pow(2, 3.0 / 12.0);
+    // CONCERT_Q: 440 * 2 ** ((0-24)/12) = 110.0 in python
+     public static final double CONCERT_A = 440.0;
+    // keyboard from "q" to " ", 37 keys
+    public static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
 
     public static void main(String[] args) {
-        /* create two guitar strings, for concert A and C */
-        GuitarString stringA = new GuitarString(CONCERT_A);
-        GuitarString stringC = new GuitarString(CONCERT_C);
+        // make sure you working wit main()
+        GuitarString[] strings = new GuitarString[keyboard.length()];
+
+        // create 37 guitar strings, from "q" to " "
+        for (int i = 0; i < keyboard.length(); i += 1){
+            double Concert = CONCERT_A * Math.pow(2, ((i - 24.0) / 12.0));
+            strings[i] = new GuitarString(Concert);
+        }
 
         while (true) {
-
             /* check if the user has typed a key; if so, process it */
             if (StdDraw.hasNextKeyTyped()) {
                 char key = StdDraw.nextKeyTyped();
-                if (key == 'a') {
-                    stringA.pluck();
-                } else if (key == 'c') {
-                    stringC.pluck();
+                for (int i = 0; i < keyboard.length(); i += 1) {
+                    if (i == keyboard.indexOf(key)){
+                        strings[i].pluck();
+                        break;
+                    }
                 }
             }
 
             /* compute the superposition of samples */
-            double sample = stringA.sample() + stringC.sample();
+            double sample = 0.0;
+            for (int i = 0; i < keyboard.length(); i += 1) {
+                sample += strings[i].sample();
+            }
 
             /* play the sample on standard audio */
             StdAudio.play(sample);
 
             /* advance the simulation of each guitar string by one step */
-            stringA.tic();
-            stringC.tic();
+            for (int i = 0; i < keyboard.length(); i += 1) {
+                strings[i].tic();
+            }
         }
     }
 }
--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -1,8 +1,10 @@
 package gh2;
 
 // TODO: uncomment the following import once you're ready to start this portion
-// import deque.Deque;
+ import deque.Deque;
 // TODO: maybe more imports
+// I would like ArrayDeque when using Java Visualizer
+ import deque.ArrayDeque;
 
 //Note: This file will not compile until you complete the Deque implementations
 public class GuitarString {
@@ -14,7 +16,8 @@ public class GuitarString {
 
     /* Buffer for storing sound data. */
     // TODO: uncomment the following line once you're ready to start this portion
-    // private Deque<Double> buffer;
+     private Deque<Double> buffer;
+     private double sample = 0.0;
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
@@ -22,6 +25,12 @@ public class GuitarString {
         //       cast the result of this division operation into an int. For
         //       better accuracy, use the Math.round() function before casting.
         //       Your should initially fill your buffer array with zeros.
+        int capacity = (int)(Math.round(SR / frequency));
+        buffer = new ArrayDeque<>();
+        while (capacity > 0){
+            buffer.addFirst(0.0);
+            capacity -= 1;
+        }
     }
 
 
@@ -35,6 +44,15 @@ public class GuitarString {
         //       other. This does not mean that you need to check that the numbers
         //       are different from each other. It means you should repeatedly call
         //       Math.random() - 0.5 to generate new random numbers for each array index.
+
+        // note: Math.random() generate double greater than 0.0 and smaller than 1.0
+        int bufferSize = buffer.size();
+        while(bufferSize > 0){
+            double r = Math.random() - 0.5;
+            buffer.removeFirst();
+            buffer.addFirst(r);
+            bufferSize -= 1;
+        }
     }
 
     /* Advance the simulation one time step by performing one iteration of
@@ -44,12 +62,17 @@ public class GuitarString {
         // TODO: Dequeue the front sample and enqueue a new sample that is
         //       the average of the two multiplied by the DECAY factor.
         //       **Do not call StdAudio.play().**
+        buffer.removeFirst();
+        double newFront = buffer.get(0);
+        sample = ((sample + newFront) / 2) * DECAY;
+        buffer.addLast(sample);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
         // TODO: Return the correct thing.
-        return 0;
+        sample = buffer.get(0);
+        return sample;
     }
 }
     // TODO: Remove all comments that say TODO when you're done.
